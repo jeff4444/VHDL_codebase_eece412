06d38fce5de371a85bfbfb12717970ebcecad828
Jeff <allojeff444@gmail.com>
2025-02-21 21:55:17 -0500

    Made vivado projects

--- eece412/homeworks/hw2_3_Allo/VHDL/hw2_3_Allo.vhd
+++ eece412/homeworks/hw2_3_Allo/VHDL/hw2_3_Allo.vhd
@@ -4,7 +4,7 @@ use ieee.std_logic_1164.all;
 entity hw2_2 is
     port (
         s : in std_logic_vector(1 downto 0);
-        z : in std_logic
+        z : out std_logic
     );
 end entity hw2_2;
 
@@ -12,7 +12,7 @@ architecture rtl of hw2_2 is
 begin
     with s select
         z <= '1' when "00",
-             '1' when "01",
+             '0' when "01",
              '1' when "10",
              '0' when others;
 end architecture rtl;
