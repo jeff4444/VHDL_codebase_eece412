bade19bad96419f09e6ca79aaa858167a92e0ddb
Jeff Mofor <allojeff444@gmail.com>
2025-03-11 22:22:43 -0400

    Added hw3

---
+++ eece412/homeworks/hw3_2_Allo/VHDL/decoder_2to4_enable.vhd
@@ -0,0 +1,19 @@
+library ieee;
+use ieee.std_logic_1164.all;
+entity decoder_2to4_enable is
+    port(
+        inp: in std_logic_vector(1 downto 0);
+        e: in std_logic;
+        outp: out std_logic_vector(3 downto 0)
+    );
+end decoder_2to4_enable;
+
+
+architecture rtl of decoder_2to4_enable is
+    begin
+    outp <= "0001" when (inp = "00" and e = '1') else
+           "0010" when (inp = "01" and e = '1') else
+           "0100" when (inp = "10" and e = '1') else
+           "1000" when (inp = "11" and e = '1') else
+           "0000";
+end rtl;›
